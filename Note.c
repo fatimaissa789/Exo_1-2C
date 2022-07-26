4fb7187875e8cd3f37adf59e02a22697a79695d5
fatima <mamefatimandiaye87@gmail.com>
2022-07-26 10:13:33 +0000

    first commit

---
+++ Note.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main () {
+    float Math [2];
+    float Fr [2];
+    float Ang [2];
+
+    char name[50];
+
+
+    const int coef1=2, coef3=4, coef2=3;//
+    float somme, moy, mention;
+
+    printf ("Veuillez saisir votre nom \n");
+    scanf ("%s",name);
+
+
+}
